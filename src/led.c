49663ffafe545cd5207b4bc5c949c90d3feddb40
Xuxingliang <xuxingliangxxl@gmail.com>
2019-01-16 10:39:07 +0800

    port to 5led.

--- src/led.c
+++ src/led.c
@@ -3,35 +3,40 @@
 #include "ezled.h"
 
 //hardware related functions
-#define LEDS4_ON()    GPIOC->ODR &= ~GPIO_PIN_3
-#define LEDS4_OFF()   GPIOC->ODR |=  GPIO_PIN_3
-#define LEDS3_ON()    GPIOB->ODR &= ~GPIO_PIN_4
-#define LEDS3_OFF()   GPIOB->ODR |=  GPIO_PIN_4
-#define LEDS2_ON()    GPIOA->ODR &= ~GPIO_PIN_3
-#define LEDS2_OFF()   GPIOA->ODR |=  GPIO_PIN_3
-#define LEDS1_ON()    GPIOD->ODR &= ~GPIO_PIN_5
-#define LEDS1_OFF()   GPIOD->ODR |=  GPIO_PIN_5
+#define LEDS5_ON()    GPIOA->ODR &= ~GPIO_PIN_3
+#define LEDS5_OFF()   GPIOA->ODR |=  GPIO_PIN_3
+#define LEDS4_ON()    GPIOB->ODR &= ~GPIO_PIN_5
+#define LEDS4_OFF()   GPIOB->ODR |=  GPIO_PIN_5
+#define LEDS3_ON()    GPIOA->ODR &= ~GPIO_PIN_1
+#define LEDS3_OFF()   GPIOA->ODR |=  GPIO_PIN_1
+#define LEDS2_ON()    GPIOB->ODR &= ~GPIO_PIN_4
+#define LEDS2_OFF()   GPIOB->ODR |=  GPIO_PIN_4
+#define LEDS1_ON()    GPIOC->ODR &= ~GPIO_PIN_3
+#define LEDS1_OFF()   GPIOC->ODR |=  GPIO_PIN_3
 #define LEDALL_OFF()  do{\
                         LEDS1_OFF();\
                         LEDS2_OFF();\
                         LEDS3_OFF();\
                         LEDS4_OFF();\
+                        LEDS5_OFF();\
                       }while(0)
 
 static void led_bsp_init(void){
-  //leds1-> PC3, leds2->PB4, leds3->PA3, leds4->PD5
-  //LEDA->PD4, LEDB->PD2, LEDC->PC7, LEDD->PC5, LEDE->PC4, LEDF->PD3, LEDG->PD1, LEDDP->PC6
+  //leds1-> PC3, leds2->PB4, leds3->PA1, leds4->PB5, leds4->PA3
+  //LEDA->PC5, LEDB->PC7, LEDC->PD4, LEDD->PD2, LEDE->PD1, LEDF->PC6, LEDG->PC4, LEDDP->PD3
   GPIO_Init(GPIOC, GPIO_PIN_3, GPIO_MODE_OUT_PP_LOW_FAST);
   GPIO_Init(GPIOB, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_FAST);
+  GPIO_Init(GPIOA, GPIO_PIN_1, GPIO_MODE_OUT_PP_LOW_FAST);
+  GPIO_Init(GPIOB, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_FAST);
   GPIO_Init(GPIOA, GPIO_PIN_3, GPIO_MODE_OUT_PP_LOW_FAST);
-  GPIO_Init(GPIOD, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_FAST);
   LEDS1_OFF();
   LEDS2_OFF();
   LEDS3_OFF();
   LEDS4_OFF();
-  GPIO_Init(GPIOC, (GPIO_Pin_TypeDef)(GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7),\
+  LEDS5_OFF();
+  GPIO_Init(GPIOC, (GPIO_Pin_TypeDef)(GPIO_PIN_5|GPIO_PIN_7|GPIO_PIN_6|GPIO_PIN_4),\
                     GPIO_MODE_OUT_PP_LOW_FAST);
-  GPIO_Init(GPIOD, (GPIO_Pin_TypeDef)(GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4),\
+  GPIO_Init(GPIOD, (GPIO_Pin_TypeDef)(GPIO_PIN_4|GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_3),\
                     GPIO_MODE_OUT_PP_LOW_FAST);  
   /* Time base configuration */
   TIM2_TimeBaseInit(TIM2_PRESCALER_1, 999);
@@ -42,17 +47,17 @@ static void led_bsp_init(void){
 }
 
 static void led_light_up(uint8_t position_set, uint8_t seg_set){
-//LEDA->PD4, LEDB->PD2, LEDC->PC7, LEDD->PC5, LEDE->PC4, LEDF->PD3, LEDG->PD1, LEDDP->PC6
-#define LED_GPIOD_MSK (GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4)
-#define LED_GPIOC_MSK (GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7)
+//LEDA->PC5, LEDB->PC7, LEDC->PD4, LEDD->PD2, LEDE->PD1, LEDF->PC6, LEDG->PC4, LEDDP->PD3
+#define LED_GPIOD_MSK (GPIO_PIN_4|GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_3)
+#define LED_GPIOC_MSK (GPIO_PIN_5|GPIO_PIN_7|GPIO_PIN_6|GPIO_PIN_4)
   //the table. input index is LEDSEGA ~ LEDSEGDP
   const uint8_t table_seg2gpiod[] = {
-    GPIO_PIN_4, GPIO_PIN_2, 0, 0,
-    0, GPIO_PIN_3, GPIO_PIN_1, 0,
+    0, 0, GPIO_PIN_4, GPIO_PIN_2, 
+    GPIO_PIN_1, 0, 0, GPIO_PIN_3,
   };
   const uint8_t table_seg2gpioc[] = {
-    0, 0, GPIO_PIN_7, GPIO_PIN_5,
-    GPIO_PIN_4, 0, 0, GPIO_PIN_6,
+    GPIO_PIN_5, GPIO_PIN_7, 0, 0,
+    0, GPIO_PIN_6, GPIO_PIN_4, 0,
   };
   uint8_t i, mask;
   uint8_t gpioc_set, gpiod_set;
@@ -86,6 +91,9 @@ static void led_light_up(uint8_t position_set, uint8_t seg_set){
     case LEDPOS4:
       LEDS4_ON();
       break;
+    case LEDPOS5:
+      LEDS5_ON();
+      break;
     default:
       break;
   }
@@ -94,7 +102,7 @@ static void led_light_up(uint8_t position_set, uint8_t seg_set){
 static uint8_t _seg_buff[32];
 ezledif_def ezledif={
   .phook = 0,
-  .count = 4,
+  .count = 5,
   .pbuff = _seg_buff,
   .szbuff = 32,
   .init = led_bsp_init,
@@ -103,5 +111,6 @@ ezledif_def ezledif={
 
 void timer2_isr(void){
   TIM2_ClearITPendingBit(TIM2_IT_UPDATE);
-  ezled_timer_isr(ezledif.phook); //do this quickly and exit isr quickly.
+  if(ezledif.phook)
+    ezled_timer_isr(ezledif.phook); //do this quickly and exit isr quickly.
 }
